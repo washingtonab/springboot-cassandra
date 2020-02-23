c7cc5ae8de8a68a0fecfdb88620e37d827a3cc82
washingtonab <washington.ab@gmail.com>
2020-02-23 12:51:40 -0300

    other commit

--- src/main/java/br/com/springboot/data/SpringbootCassandraApplication.java
+++ src/main/java/br/com/springboot/data/SpringbootCassandraApplication.java
@@ -1,16 +1,46 @@
 package br.com.springboot.data;
 
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration;
+import org.springframework.data.cassandra.core.CassandraOperations;
+import org.springframework.data.cassandra.core.CassandraTemplate;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { CassandraDataAutoConfiguration.class })
-public class SpringbootCassandraApplication {
+public class SpringbootCassandraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCassandraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Cluster cluster = Cluster.builder()
+				 .addContactPoints("localhost")
+				 .withPort(9042)
+				 .withCredentials("username", "password")
+				 .withoutJMXReporting()
+				 .build();
+		Session session = cluster.connect("keyspace");
+		CassandraOperations template = new CassandraTemplate(session);
+
+		String one = template.selectOne(QueryBuilder.select("column").from("table").allowFiltering().where(QueryBuilder.eq("column", "123")), String.class);
+
+		System.out.println(one);
+
+		List<String> lista = template.select("select column from table limit 100", String.class);
+
+		System.out.println(lista.size());
 	}
 
 }
